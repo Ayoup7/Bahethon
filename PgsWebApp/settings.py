dc4aeeef38eafea0bc3b0abae5aecee6210e45e8
Ayoup7 <itpg92@uob.edu.ly>
2023-01-14 22:39:12 +0200

    Complete projects

--- PgsWebApp/settings.py
+++ PgsWebApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*qf@yvqwvbc58%#kxydd$a8)8e2vp+b2k$*5^@)4&k)^%gw28d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
