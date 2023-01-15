20366fa970e5cf05c843ba33c95b672c407ac0b3
Ayoup7 <itpg92@uob.edu.ly>
2023-01-15 09:09:54 +0200

    Change directory

--- Pgs/pgs/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PgsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pgs'
--- build_files.sh
+++
@@ -1,4 +0,0 @@
-echo " BUILD START"
-Python3.10.9 -m install -r requirements.txt
-Python3.10.9 manage.py collectstatic --noinput --clear
-echo " BUILD END"
