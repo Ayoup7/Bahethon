bb8bd75c42a5ccbfc8dac832ffbf5b55b19e8fcd
Ayoup7 <itpg92@uob.edu.ly>
2023-01-15 15:03:18 +0200

    last Commit

--- PgsWebApp/settings.py
+++ PgsWebApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*qf@yvqwvbc58%#kxydd$a8)8e2vp+b2k$*5^@)4&k)^%gw28d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
 
 # Application definition
@@ -118,9 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'PgsWebApp/static')]
 
 # MEDIA 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- static/js/data.js
+++ static/js/data.js
@@ -43,3 +43,4 @@ department.addEventListener('change', e=>{
 })
 
 
+
