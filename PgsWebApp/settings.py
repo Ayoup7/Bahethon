6c1e1a1f19ec25e464c6bdad7bafe36229acbbc6
Ayoup7 <itpg92@uob.edu.ly>
2023-01-15 20:21:18 +0200

    last Commit

--- PgsWebApp/settings.py
+++ PgsWebApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*qf@yvqwvbc58%#kxydd$a8)8e2vp+b2k$*5^@)4&k)^%gw28d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
